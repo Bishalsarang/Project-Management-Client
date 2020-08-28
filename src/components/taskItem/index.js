dee1a798dbc553e33cd7c237a433f517928a9e99
Bishalsarang <bishalsarang@gmail.com>
2020-08-28 23:07:10 +0545

    Fix useEffect can't be async

--- src/components/taskItem/index.js
+++ src/components/taskItem/index.js
@@ -8,20 +8,26 @@ import fetcher from '../../utils/axiosIntercept';
 const TaskItem = ({ id, title, description, deadline }) => {
   const [assignedUser, setAssignedUser] = useState('');
 
-  useEffect(async () => {
-    const users = await fetcher.get(API_TASKS_URL + id + '/users');
+  useEffect(() => {
+    const fetchData = async () => {
+      const users = await fetcher.get(API_TASKS_URL + id + '/users');
 
-    const userId = users.data.data.filter((user) => user.is_assigned)[0].user_id;
+      const userId = users.data.data.filter((user) => user.is_assigned)[0].user_id;
 
-    const user = await fetcher.get(API_USERS_URL + userId);
+      const user = await fetcher.get(API_USERS_URL + userId);
 
-    setAssignedUser(user.data.data[0].username);
+      setAssignedUser(user.data.data[0].username);
+    };
+
+    fetchData();
   }, []);
 
   return (
     <Card className="task-item">
       <Card.Body>
-        <Card.Title>{title}</Card.Title>
+        <Card.Title className="text-center">
+          <h4 className="text-primary">{title}</h4>
+        </Card.Title>
 
         <Card.Text>{description}</Card.Text>
         <Card.Text>Assigned to: {assignedUser}</Card.Text>
